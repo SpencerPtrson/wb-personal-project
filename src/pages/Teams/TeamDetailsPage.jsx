0ea156ebe6a162af7738b904e328592ec2f49936
Spencer Peterson <spencerpeterson.wi@gmail.com>
2023-11-14 13:00:08 -0700

    Adding a pokemon to a team now sends you to the team details page

--- src/pages/Teams/TeamDetailsPage.jsx
+++ src/pages/Teams/TeamDetailsPage.jsx
@@ -1,7 +1,11 @@
 import { useLoaderData } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import TeamPokemonInstanceTable from '../../components/General Data Viewing/PokemonTeamViewer/TeamPokemonInstanceTable';
+import CreatePokemonInstanceButton from '../../components/PokemonInstanceManipulation/CreatePokemonInstanceButton';
 
 export default function TeamDetailsPage() {
+  const email = useSelector(state => state.user.email);
+
   let { team } = useLoaderData();
   console.log("Team Details Page - team object:", team);
   console.log("Team Pokemon Instances:", team.pokemonInstances);
@@ -13,6 +17,10 @@ export default function TeamDetailsPage() {
         : <></>
       }
       <TeamPokemonInstanceTable instanceList={team.pokemoninstances} isTeamView={true} creatorEmail={team.user.email}/>
+      { team.user.email === email
+        ? <CreatePokemonInstanceButton teamId={team.teamId}/>
+        : <></>
+      }
     </>
   );
 }
